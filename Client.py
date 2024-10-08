a6b61584208d82ca4cdcdb43a6d611153cdef901
brunomdr2005 <116674767+brunomdr2005@users.noreply.github.com>
2024-10-08 20:33:44 -0300

    Update Client.py

--- Client.py
+++ Client.py
@@ -5,6 +5,6 @@ PORT = 1234
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b"Papagaio")   # o "B" significa bytes
+    s.sendall(b"Teste")   # o "B" significa bytes
     data = s.recv(1024)
     print(f"recebido: {data!r}")  # !r transforma b em str
