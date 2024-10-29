87af94b0c56b7989b7ceaf9b175bb496dd947a18
brunomdr2005 <116674767+brunomdr2005@users.noreply.github.com>
2024-10-29 19:01:59 -0300

    Update Client.py

--- Client.py
+++ Client.py
@@ -5,6 +5,7 @@ PORT = 1234
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b"Teste")   # o "B" significa bytes
+    print("Digite algo: ")
+    n = input()
     data = s.recv(1024)
     print(f"recebido: {data!r}")  # !r transforma b em str
